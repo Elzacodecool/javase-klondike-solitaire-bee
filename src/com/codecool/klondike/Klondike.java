86cddfdda70c3037308b22b74952d61958c7ed89
Bart <bartpetka@gmail.com>
2018-03-27 14:04:11 +0200

    Implement theme swap method

--- src/com/codecool/klondike/Klondike.java
+++ src/com/codecool/klondike/Klondike.java
@@ -5,10 +5,16 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import javafx.scene.control.Button;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+
+
 public class Klondike extends Application {
 
     private static final double WINDOW_WIDTH = 1400;
     private static final double WINDOW_HEIGHT = 900;
+    Button changeThemeButton;
 
     public static void main(String[] args) {
         launch(args);
@@ -20,6 +26,8 @@ public class Klondike extends Application {
         Game game = new Game();
         game.setTableBackground(new Image("/table/green.png"));
 
+        initializeButtons(game);
+        
         primaryStage.setTitle("Klondike Solitaire");
         primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
         if(game.isGameWon()) {
@@ -28,4 +36,16 @@ public class Klondike extends Application {
         primaryStage.show();
     }
 
+    public void swapThemes(Game game){
+        game.setTableBackground(new Image("/table/red.png"));
+    }
+
+    public void initializeButtons(Game game){
+        changeThemeButton = new Button("red theme");
+        changeThemeButton.setLayoutY(30);
+        changeThemeButton.setOnAction(e -> {
+            swapThemes(game);
+        });
+        game.getChildren().add(changeThemeButton);
+    }
 }
