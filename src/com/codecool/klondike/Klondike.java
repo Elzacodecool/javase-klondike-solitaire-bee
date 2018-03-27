fc4653ee52550c51016e0f307faecc1e69e28b0b
Bart <bartpetka@gmail.com>
2018-03-27 19:52:57 +0200

    Move startGame into separate function

--- src/com/codecool/klondike/Klondike.java
+++ src/com/codecool/klondike/Klondike.java
@@ -15,6 +15,7 @@ public class Klondike extends Application {
     private static final double WINDOW_WIDTH = 1400;
     private static final double WINDOW_HEIGHT = 900;
     Button changeThemeButton;
+    Button newGame;
 
     public static void main(String[] args) {
         launch(args);
@@ -22,24 +23,34 @@ public class Klondike extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        startGame(primaryStage);
+    }
+
+    public void startGame(Stage primaryStage){
         Card.loadCardImages();
         Game game = new Game();
         game.setTableBackground(new Image("/table/green.png"));
 
-        initializeButtons(game);
+        initializeButtons(game, primaryStage);
         
         primaryStage.setTitle("Klondike Solitaire");
         primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
         primaryStage.show();
     }
 
-
-    public void initializeButtons(Game game){
+    public void initializeButtons(Game game, Stage primaryStage){
+        newGame = new Button("New Game");
+        newGame.setLayoutY(60);
+        newGame.setOnAction(e -> {
+            primaryStage.close();
+            startGame(primaryStage);
+        });
         changeThemeButton = new Button("Themes");
         changeThemeButton.setLayoutY(30);
         changeThemeButton.setOnAction(e -> {
             ThemesBox.display("Themes", "", game);
         });
         game.getChildren().add(changeThemeButton);
+        game.getChildren().add(newGame);
     }
 }
