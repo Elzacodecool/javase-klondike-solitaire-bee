9525a5c0444b8a12df7d3b4409eacb564ac2f52f
Bart <bartpetka@gmail.com>
2018-04-05 19:26:46 +0200

    Implement reading/writting game mode settings methods

--- src/com/codecool/klondike/Common.java
+++ src/com/codecool/klondike/Common.java
@@ -14,10 +14,12 @@ public class Common{
         try {
             BufferedReader br = new BufferedReader(new FileReader("resources/theme.txt"));
             String line = br.readLine();
+
             while (line != null) {
                 themeSettings.add(line);
                 line = br.readLine();
             }
+            br.close();
         }
         catch (IOException e) {
             System.out.println("Settings file damaged");
@@ -31,6 +33,33 @@ public class Common{
             for (String setting : newSettings){
                 fw.write(setting);
             }
+            fw.close();
+        }
+        catch (IOException e){
+            System.out.println("Cannot find settings.");
+        }
+    }
+
+    public static int loadGameMode() {
+        int mode = 1;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("resources/gameMode.txt"));
+            String line = br.readLine();
+            mode = Integer.parseInt(line);
+            br.close();
+        }
+        catch (IOException e) {
+            System.out.println("Settings file damaged");
+            }
+        return mode;
+        }
+
+    
+    public static void saveGameMode(int cardsMode){
+        try{
+            FileWriter fw = new FileWriter("resources/gameMode.txt");
+            String setting = String.valueOf(cardsMode);
+            fw.write(setting);
             fw.close();
         }
         catch (IOException e){
