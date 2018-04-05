290321bf43a9c68b42bc7eb50f2e110912cb0ea8
Bart <bartpetka@gmail.com>
2018-04-05 19:25:58 +0200

    Replace Theme settings by Settings with game mode selection

--- src/com/codecool/klondike/SettingsBox.java
+++ src/com/codecool/klondike/SettingsBox.java
@@ -8,7 +8,7 @@ import javafx.scene.control.*;
 import javafx.geometry.*;
 import java.util.ArrayList;
 
-public class ThemesBox {
+public class SettingsBox {
 
     public static void display(String title, String message, Game game) {
         ArrayList<String> settings = new ArrayList<String>();
@@ -17,10 +17,11 @@ public class ThemesBox {
 
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
-        window.setMinWidth(250);
+        window.setMinWidth(300);
+        window.setMinHeight(500);
 
-        Button changeRed = new Button("DARK THEME");
-        changeRed.setOnAction(e -> {
+        Button darkTheme = new Button("DARK THEME");
+        darkTheme.setOnAction(e -> {
             System.out.println("Pressed dark theme button");
             settings.add("table/red.png\n");
             settings.add("card_images/darkTheme/");
@@ -41,13 +42,41 @@ public class ThemesBox {
             window.close();
         });
 
+        Button singleCardMode = new Button("Single card");
+        singleCardMode.setOnAction(e -> {
+            System.out.println("Set single card game mode");
+            Common.saveGameMode(1);
+            window.close();
+        });
+
+        Button doubleCardMode = new Button("Two cards");
+        doubleCardMode.setOnAction(e -> {
+            System.out.println("Set double cards game mode");
+            Common.saveGameMode(2);
+            window.close();
+        });
+
+        Button threeCardMode = new Button("Three cards");
+        threeCardMode.setOnAction(e -> {
+            System.out.println("Set three cards game mode");
+            Common.saveGameMode(3);
+            window.close();
+        });
+
         Label label = new Label();
-        label.setText(message);
+        label.setText("Themes settings:");
+
+        Label gameModeSelect = new Label();
+        gameModeSelect.setText("Select game mode (requires restart):");
 
         VBox layout = new VBox(10);
         layout.getChildren().add(label);
-        layout.getChildren().add(changeRed);
+        layout.getChildren().add(darkTheme);
         layout.getChildren().add(lightTheme);
+        layout.getChildren().add(gameModeSelect);
+        layout.getChildren().add(singleCardMode);
+        layout.getChildren().add(doubleCardMode);
+        layout.getChildren().add(threeCardMode);
         layout.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(layout);
